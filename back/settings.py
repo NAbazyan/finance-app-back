48049035758cff877c57142d26227106671a272c
NAbazyan <novelabazyan3@gmail.com>
2025-11-21 10:37:34 +0400

    created-front

--- back/settings.py
+++ back/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'back.wsgi.application'
 #     )
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 SECUREPROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -113,11 +113,11 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://banking-front-opal.vercel.app/"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "https://banking-front-opal.vercel.app/"
+# ]
 
 CORS_TRUSTED_ORIGINS =  [
     "http://localhost:5173",
