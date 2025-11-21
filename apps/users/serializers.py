246279559936ca09f269e687d2354fa29437b035
NAbazyan <novelabazyan3@gmail.com>
2025-11-21 11:33:49 +0400

    front-change

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -19,9 +19,6 @@ class RegisterSerializer(serializers.ModelSerializer):
             username = validated_data['username'],
             password = validated_data['password'],
             email = validated_data['email'],
-            # full_name = validated_data.get('full_name', ""),
-            # phone_number = validated_data.get('phone_number', '')
-            # avatar = validated_data.get('avatar', None),
         )
         user.set_password(validated_data["password"])
         user.save()
